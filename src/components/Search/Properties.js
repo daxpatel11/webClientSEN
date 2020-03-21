52e95edc75a2f734334e4fa0e1c3a6bbe6568edf
Dax Patel <32451078+Daxpatel1105@users.noreply.github.com>
2020-03-21 12:20:45 +0530

    Update Properties.js

--- src/components/Search/Properties.js
+++ src/components/Search/Properties.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import App from './App.css';
 
 
 class Properties extends Component{
@@ -26,27 +27,115 @@ class Properties extends Component{
         return(<div> loading....</div>)
 
         return(
-           <div><h1> properties</h1>
           
-            <div><ul className="person" styled={App}>
+          
+            <div className="container" >
+                <div class="row">
+                <div className="col-lg-8 col-md-12">
+                <div class="option-bar">
+            <h1 style={{fontSize:20 , textAlign:'center'}}> Properties List</h1>
+                
+
+          </div>
          {this.state.properties.slice(0,this.state.visible).map((property,i) => (
-             <div key ={`some-property -${i}`}>
-                   <img src={property.picture.large}/>
-                 <div>AreaType: {property.Areatype}</div>
-                 <div> Location : {property.location.street.number},  
-                {property.location.street.name}</div>
-                <div>city:{property.location.city}</div>
-                <div>state:{property.location.state}</div>
-                <div>country:{property.location.country}</div>
-                <div>postcode:{property.location.postcode}</div>
+             <div class="property-box-5">
+                 <div class="row">
+                     <div class="col-lg-5 col-md-5 col-pad">
+             <div  key ={`some-property -${i}`}>
+                 
+                  <div className="property-thumbnail">
+                      
+                       <  img style={{height:150}}src={property.picture.large}/></div> 
+                           
+                          <div class="detail">
+                              <div class="id">property name</div>
+                 <div class="title">{property.Areatype}</div>
+                 <div class="location fa fa-map-marker">
+                  <i id="address" class="fa fa-map-marker">
+                  {property.location.street.number}, {property.location.street.name}, {property.location.city},
+                {property.location.state},
+                {property.location.country},-
+                {property.location.postcode}
+                </i>
+                </div>
+                <div class="owner">
+                <i class="flaticon-bed">
+                            
+                            </i> Contact</div> 
+                <div class="totalsize">
+                <i class="flaticon-bed">
+                    </i>sq feet 50</div> 
+                    <div class="price">
+                        1000000</div> 
+                <ul class="facilities-list clearfix">
+                    <li>
+                         
+                        <i class="flaticon-bed">
+                            
+                        </i>
+                        2 beds
+                    </li>
+                    <li>
+                         
+                        <i class="flaticon-bed">
+                            
+                        </i>
+                        3 bathrooms
+                    </li>
+                    <li>
+                         
+                         <i class="flaticon-bed">
+                             
+                         </i>
+                         3 Balconies
+                        </li>
+                    
+                     <li>
+                         
+                         <i class="flaticon-bed">
+                             
+                         </i>
+                         Furnished
+                     </li>
+                     
+                 </ul>
+                 <ul class="facilities-list clearfix"> 
+                 <li>
+                
+                    Distance to nearest Gym:
+                     </li>
+                     <li>
+                
+                    Distance to nearest School:
+                     </li>
+                     <li>
+               
+                    Distance to nearest Hospital:
+                     </li>
+                     </ul>
+
+
+                
+                </div>
+                </div>
+                        </div>    
+              </div>
+              </div>
               
-             </div>
+              
+              
+            
+             
          ))
         }
-         </ul>
+         
          </div>
-         <button onClick={this.loadmore}>load more </button>
+         <div class="pagination">
+         <button primary style={{size:20,backgroundColor:'tranparent' ,textAlign:'center'}}  onClick={this.loadmore}>load more </button>
          </div>
+         </div>
+         </div>
+         
         );
     }
 
