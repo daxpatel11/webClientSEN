19bf01c7ee1e70b01886373b6f90e170b271e432
Dax Patel <32451078+Daxpatel1105@users.noreply.github.com>
2020-03-22 14:59:13 +0530

    Create app.js

---
+++ src/components/Search/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Properties from './Properties.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+       <Properties />
+      </div>
+    );
+  }
+}
+
+export default App;
